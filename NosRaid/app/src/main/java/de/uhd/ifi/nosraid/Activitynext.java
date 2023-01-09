e3bd9685742abc3cf52d34a14615dd8127300a9e
Tobias.S <sieber.tobias8@gmail.com>
2023-01-09 20:14:48 +0100

    delted

---
+++ NosRaid/app/src/main/java/de/uhd/ifi/nosraid/Activitynext.java
@@ -0,0 +1,14 @@
+package de.uhd.ifi.nosraid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activitynext extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_activitynext);
+    }
+}
